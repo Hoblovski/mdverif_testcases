8e553f1985a4783841927ace08d5c7da3d49bda9
Yu Jian <lkmcfj@outlook.com>
2020-12-10 13:41:15 +0800

    fix typo: -> to -->

--- testcases/step2/par_ensures.c
+++ testcases/step2/par_ensures.c
@@ -1,7 +1,7 @@
 // 可选：让形参作用域也囊括 ensures（ensures 中的形参表示形参初始值）
 int f1(int x, int y)
     requires x == 5
-    ensures y == 6 -> result == 11
+    ensures y == 6 --> result == 11
 {
     int z;
     z = y;
