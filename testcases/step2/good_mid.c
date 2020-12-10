a844a924866197e0d76c7282f8a0ac6b9f145a01
Yu Jian <lkmcfj@outlook.com>
2020-12-10 14:24:28 +0800

    add l >= 0 in good_mid

--- testcases/step2/good_mid.c
+++ testcases/step2/good_mid.c
@@ -1,9 +1,10 @@
 // 用关键字 result 表示返回值
-int mid_bad(int l, int r)
+int mid_good(int l, int r)
+    requires l >= 0
     requires l <= r
     ensures l <= result && result <= r
 {
-    return l + (r-l)/2;
+    return l + (r - l) / 2;
 }
 
 int main() {}
